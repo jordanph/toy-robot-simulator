9ad89b06980a3cf8fffa492cb24b6cadf745d4ed
Jordan Holland <holland.j@wehi.edu.au>
2018-05-17 20:04:48 +1000

    - Added intialiser for board - Added unit tests for initialiser

--- ToyRobotSimulator.Tests/BoardTests.cs
+++ ToyRobotSimulator.Tests/BoardTests.cs
@@ -6,6 +6,40 @@ namespace ToyRobotSimulator.Tests
 {
     public class BoardTests
     {
-        
+        [Fact]
+        public void WidthLessThanZero()
+        {
+            try
+            {
+                int width = 0;
+                int height = 5;
+
+                var board = new Board(width, height);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return;
+            }
+
+            Assert.True(false, "Width cannot be less or equal to zero.");
+        }
+
+        [Fact]
+        public void HeightLessThanZero()
+        {
+            try
+            {
+                int width = 5;
+                int height = 0;
+
+                var board = new Board(width, height);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return;
+            }
+
+            Assert.True(false, "Height cannot be less or equal to zero.");
+        }
     }
 }
--- ToyRobotSimulator/Models/Board.cs
+++ ToyRobotSimulator/Models/Board.cs
@@ -6,6 +6,15 @@ namespace ToyRobotSimulator.Models
 {
     public class Board
     {
+        public Board(int width, int height)
+        {
+            if (width < 0 || height < 0)
+            {
+                throw new ArgumentOutOfRangeException("Both width and height values must be greater than 0.");
+            }
+        }
 
+        public int Width { get; set; }
+        public int Height { get; set; }
     }
 }
